88509a6195bf17dfed0d043ecd3fa216024dfa02
Todd <danny.todd@accenture.com>
2019-07-08 14:29:56 +0100

    preparing for dynamic content

--- jobs/app.py
+++ jobs/app.py
@@ -6,7 +6,7 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g, '_connection', default=None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
@@ -19,6 +19,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+
     cursor.close()
     return results
 
